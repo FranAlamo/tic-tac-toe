a8653d7063a814b907b094b139802d9f9cfbdea3
FranAlamo <alamofrancis@gmail.com>
2023-06-27 15:39:08 -0300

    square clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import "./App.css";
-import { Box } from "./components/Box.jsx";
-export default function Square() {
+import Square from "./components/Square.jsx";
+export default function Board() {
   return (
     <>
-      <Box />
+      <h1 className="title">Tic Tac Toe By Fran</h1>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
     </>
   );
 }
